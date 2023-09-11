1c80c09e3741e839e31142912b02cfade93fd8d7
Yiyo Zapata <yiyo.zapata@etoolinnovation.com>
2023-09-11 12:30:36 +0200

    Manejo de errores

--- microblog/config.py
+++ microblog/config.py
@@ -5,3 +5,9 @@ class Config(object):
     MONGO_URI = os.environ.get('MONGODB_URI') or \
         'mongodb://localhost:27017/microblog'
     USER_ID_FIELD = '_id'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
